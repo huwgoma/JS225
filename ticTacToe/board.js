4a8f881b9b8ec35202ed877e4f4da64f00cc71ba
Hugo Ma <huwgoma@gmail.com>
2025-10-29 13:16:42 -0400

    Add Board initialization logic

--- ticTacToe/board.js
+++ ticTacToe/board.js
@@ -1,5 +1,10 @@
+// Class for the TTT board; internally represented as an Array.
+// [ _, _, _,     [ 1, 2, 3, 
+//   _, _, _,       4, 5, 6,
+//   _, _, _, ]     7, 8, 9 ]
+
 class Board {
-  
+  #grid = new Array(9);
 }
 
 module.exports = Board;
