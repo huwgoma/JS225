157b1f9bbe54d6fc01c702755ca7b61b42f71e58
Hugo Ma <huwgoma@gmail.com>
2025-10-29 14:54:03 -0400

    Add Board#emptyAt method - returns a boolean representing whether the board is empty at the given square

--- ticTacToe/board.js
+++ ticTacToe/board.js
@@ -4,13 +4,26 @@
 //   _, _, _, ]     7, 8, 9 ]
 
 class Board {
-  #grid = Array.from({ length: 9 });
+  #grid;
+
+  constructor(sideLength = 3) {
+    this.#grid = Array.from({ length: sideLength ** 2 });
+  }
 
   emptySquares() {
     return this.#grid.reduce((result, square, index) => {
       if (square === undefined) result.push(index + 1);
       return result;
     }, []);
+  }
+
+  emptyAt(square) {
+    square = Number(square);
+    if (Number.isNaN(square) || square < 1 || square > this.#grid.length) {
+      return false;
+    }
+
+    return this.#grid[square - 1] === undefined;
   }
 }
 
