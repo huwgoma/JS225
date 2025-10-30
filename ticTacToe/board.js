a18e2d181f6ed153d0c2f13b3d23b3ec984ddcb8
Hugo Ma <huwgoma@gmail.com>
2025-10-30 10:28:39 -0400

    Add Board#squareToIndex method for converting 1-indexed square inputs to rowIndex/colIndex coordinates; refactor #markAt and #emptyAt to accommodate 2-D grid structure.

--- ticTacToe/board.js
+++ ticTacToe/board.js
@@ -1,8 +1,4 @@
-// Class for the TTT board; internally represented as an Array.
-// [ _, _, _,     [ 1, 2, 3, 
-//   _, _, _,       4, 5, 6,
-//   _, _, _, ]     7, 8, 9 ]
-
+// Class for the TTT board; internally represented as a 2-D Array.
 class Board {
   static sideLength = 3;
   static maxSquare = Board.sideLength ** 2;
@@ -37,25 +33,39 @@ class Board {
   }
 
   emptySquares() {
-    return this.#grid.flat().reduce((result, square, index) => {
-      if (square === undefined) result.push(index + 1);
-      return result;
+    return this.#grid.flat().reduce((memo, square, index) => { 
+      if (square === undefined) memo.push(index + 1);
+
+      return memo;
     }, []);
   }
 
   emptyAt(square) {
-    if (!this.#squareIsValid(Number(square))) return false;
+    if (this.#invalidSquare(square)) return false;
 
-    return this.#grid[square - 1] === undefined;
+    let [ rowIndex, colIndex ] = this.#squareToIndex(square);
+    return this.#grid[rowIndex][colIndex] === undefined;
   }
 
   markAt(square, mark) {
-    this.#grid[square - 1] = mark;
+    let [ rowIndex, colIndex ] = this.#squareToIndex(square);
+    this.#grid[rowIndex][colIndex] = mark;
   }
 
-  #squareIsValid(square) {
-    return !(Number.isNaN(square)) && 
-      square >= 1 && square <= Board.maxSquare;
+  #squareToIndex(square) {
+    let sqNum = Number(square);
+
+    let squareIndex = sqNum - 1;
+    let rowIndex = Math.floor(squareIndex / Board.sideLength);
+    let colIndex = squareIndex % Board.sideLength;
+
+    return [rowIndex, colIndex];
+  }
+
+
+  #invalidSquare(square) {
+    let sqNum = Number(square);
+    return Number.isNaN(sqNum) || sqNum < 1 || sqNum > Board.maxSquare;
   }
 }
 
