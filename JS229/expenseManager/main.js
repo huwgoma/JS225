80e4b169e17a38385163970daa75e1b11e67c8a8
Hugo Ma <huwgoma@gmail.com>
2025-11-25 12:43:03 -0500

    Add test usage of adding expenses to main.js

--- JS229/expenseManager/main.js
+++ JS229/expenseManager/main.js
@@ -6,7 +6,10 @@ tomorrow.setDate(tomorrow.getDate() + 1);
 
 let manager = new ExpenseManager();
 
-// Invalid Expense -> Logs "Could not add expense: Invalid expense data."
-manager.addExpense(-1, today, 'Food');    
-manager.addExpense(13, tomorrow, 'Food');
-manager.addExpense(13, today);
+// Usage:
+// Adding Invalid Expenses logs "Could not add expense: Invalid expense data."
+manager.addExpense(-1, today, 'Food');    // Invalid Price
+manager.addExpense(13, tomorrow, 'Food'); // Invalid Date
+
+manager.addExpense(13, today);                  // Invalid Category -> "Invalid expense category."
+manager.addExpense(13, today, 'Bad Category');  // Invalid Category -> "Invalid expense category."
