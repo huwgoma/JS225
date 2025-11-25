3fc14a1f59bdbe44534cd79093efe4f528411572
Hugo Ma <huwgoma@gmail.com>
2025-11-25 10:56:47 -0500

    JS229 EM - Add test for Expense verifying that Expenses cannot be modified after creation

--- JS229/expenseManager/expense.test.js
+++ JS229/expenseManager/expense.test.js
@@ -1,10 +1,30 @@
 const Expense = require('./expense');
 
-test('valid expense has id/amount/date/category', () => {
-  let today = new Date();
-  today.setHours(0, 0, 0, 0);
+const today = new Date();
+today.setHours(0, 0, 0, 0);
 
+const yesterday = new Date(today);
+yesterday.setDate(yesterday.getDate() - 1);
+
+const tomorrow = new Date(today);
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+test('valid expense has id/amount/date/category', () => {
   let chipotle = new Expense(1, 13, today, 'Food');
+
+  expect(chipotle.id).toBe(1);
+  expect(chipotle.amount).toBe(13);
+  expect(chipotle.date).toBe(today);
+  expect(chipotle.category).toBe('Food');
+});
+
+test('expenses cannot be altered', () => {
+  let chipotle = new Expense(1, 13, today, 'Food');
+  
+  chipotle.id = 10;
+  chipotle.amount = 130;
+  chipotle.date = yesterday;
+  chipotle.category = 'Not Food';
 
   expect(chipotle.id).toBe(1);
   expect(chipotle.amount).toBe(13);
